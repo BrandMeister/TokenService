e36d0f2cfa762de96197a11801092277af9450f1
Artem Prilutskiy <performer@aesyle>
2018-12-01 23:22:23 +0300

    Initial import

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+  require_once('vendor/autoload.php');
+
+  $data = file_get_contents('service-account.json');
+  $parameters = json_decode($data, true);
+
+  $client = new Google_Client();
+  $client->setAuthConfig($parameters);
+  $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
+  $client->setApplicationName('TokenService');
+  $client->useApplicationDefaultCredentials();
+  $client->refreshTokenWithAssertion();
+  $token = $client->getAccessToken();
+
+  $response = array(
+    'application' => $parameters['project_id'],
+    'token'       => $token['access_token'],
+    'expires'     => $token['expires_in']
+	);
+
+  $data = json_encode($response);
+
+  header("Content-Type: application/json");
+  print($data);
+
+?>
